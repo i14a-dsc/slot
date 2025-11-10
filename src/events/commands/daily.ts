c1d25c0ed744bf4c76364629cd48508af83b9083
i14a-dsc <dev.i14a.dsc@gmail.com>
2025-11-10 12:35:24 +0900

    chore: update localization

--- src/events/commands/daily.ts
+++ src/events/commands/daily.ts
@@ -5,7 +5,7 @@ import { addCoin, claimDailyBonus, randomInt } from "../../util/utilities";
 export const command: Command = {
   data: {
     name_localizations: {
-      ja: "デイリーボーナス",
+      ja: "デイリー",
     },
     name: "daily",
     description_localizations: {
@@ -21,7 +21,8 @@ export const command: Command = {
 
     if (!daily.success) {
       await interaction.reply({
-        content: "You already claimed your Daily reward today!",
+        content:
+          "すでにデイリーボーナスを受け取りました。また明日来てください！",
         flags: [64],
       });
       return;
@@ -31,9 +32,9 @@ export const command: Command = {
 
     await interaction.reply({
       content: replace([
-        `You claimed your Daily reward!`,
-        `You received ${rewardCoins}%coin!`,
-        `Now your current amount of coins is ${current}%coin.`,
+        `デイリーボーナスを受け取りました！`,
+        `${rewardCoins}%coin を獲得しました！`,
+        `現在の所持コインは ${current}%coin です。`,
       ]),
       flags: [64],
     });
